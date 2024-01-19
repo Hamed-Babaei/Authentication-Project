0891b348d894cb1e88e838497a538f82803b37f3
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-01-19 11:39:27 +0330

    RBAC is developed for P-Admin Page

---
+++ next-auth/pages/p-admin/index.js
@@ -0,0 +1,45 @@
+import { getSession } from "next-auth/react";
+import React from "react";
+import UserModel from "@/models/User";
+
+export default function index({ user }) {
+  return (
+    <div>
+      <h1>
+        Welcome to Admin Panel , {user.firstName} - {user.lastName}
+      </h1>
+    </div>
+  );
+}
+
+//? SSR Route Protection => (RBAK)
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+  console.log("Session => ", session);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+      },
+    };
+  }
+  const user = await UserModel.findOne(
+    { email: session.user.email },
+    "firstName lastName role"
+  );
+
+  if (user.role === "USER") {
+    return {
+      redirect: {
+        destination: "/dashboard",
+      },
+    };
+  }
+
+  return {
+    props: {
+      user: JSON.parse(JSON.stringify(user)),
+    },
+  };
+}
